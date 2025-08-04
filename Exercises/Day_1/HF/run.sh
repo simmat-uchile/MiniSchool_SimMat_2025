b33c1f86d9a23518655c721d137d52e9c4884a1a
simmat-uchile <simmat.uchile@gmail.com>
2025-08-04 03:02:56 -0400

    Add files via upload

--- Exercises/Day_1/HF/run.sh
+++ Exercises/Day_1/HF/run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=G09
 #SBATCH --partition=labs
-#SBATCH -c 1 #numero de CPUs
+#SBATCH -c 2 #numero de CPUs
 #SBATCH --output=example.out
 #SBATCH --error=example.err
-#SBATCH --mem=1000
+#SBATCH --mem=1500
 #SRUN --export=ALL
 
 ml intel/2019b 
